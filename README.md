7f3427e0b4de41ee825ae774aad62ed13b5c7ffc
Mason Lee <jacklee_l@hotmail.com>
2024-06-18 20:57:15 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,12 @@
+
+# HtmxChat
+
+This demonstrates a working HTMX Websocket usage. It uses Ratchet as Websocket server.
+
+## Steps to install and run
+
+ - composer install
+ - docker-compose up -d
+ - With web browser, open at least two tabs or windows: http://127.0.0.1/
+ - You should see your messages passed between the instances.
+
---
+++ src/Chat.php
@@ -0,0 +1,45 @@
+<?php
+namespace HtmxChat;
+use Ratchet\MessageComponentInterface;
+use Ratchet\ConnectionInterface;
+
+class Chat implements MessageComponentInterface {
+    protected $clients;
+
+    public function __construct() {
+        $this->clients = new \SplObjectStorage;
+    }
+
+    public function onOpen(ConnectionInterface $conn) {
+        // Store the new connection to send messages to later
+        $this->clients->attach($conn);
+
+        echo "New connection! ({$conn->resourceId})\n";
+    }
+
+    public function onMessage(ConnectionInterface $from, $msg) {
+        $numRecv = count($this->clients) - 1;
+        echo sprintf('Connection %d sending message "%s" to %d other connection%s' . "\n"
+            , $from->resourceId, $msg, $numRecv, $numRecv == 1 ? '' : 's');
+
+        $msgArr = json_decode($msg, true);
+
+        // send to each client connected
+        foreach ($this->clients as $client) {
+            $client->send("<div id='chat_history' hx-swap-oob='afterbegin'><div>{$msgArr['chat_typed']}</div>\n</div>");
+        }
+    }
+
+    public function onClose(ConnectionInterface $conn) {
+        // The connection is closed, remove it, as we can no longer send it messages
+        $this->clients->detach($conn);
+
+        echo "Connection {$conn->resourceId} has disconnected\n";
+    }
+
+    public function onError(ConnectionInterface $conn, \Exception $e) {
+        echo "An error has occurred: {$e->getMessage()}\n";
+
+        $conn->close();
+    }
+}
